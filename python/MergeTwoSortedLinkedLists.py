6df9ecf52d4d26f65b00a8bf2b5daf7680b55b28
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-01-15 21:01:16 +0100

    Implement merge two list algorithm

---
+++ python/MergeTwoSortedLinkedLists.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    res = ListNode()
+    tail = res
+    while list1 and list2:
+        if list1.val <= list2.val:
+            tail.next = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+        tail = tail.next
+
+    if list1:
+        tail.next = list1
+    elif list2:
+        tail.next = list2
+
+    return res.next
+
+def printNode(node: Optional[ListNode]) -> None:
+    curr = node
+    while curr:
+        print(curr.val)
+        curr = curr.next
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(4)
+
+l2 = ListNode(1)
+l2.next = ListNode(3)
+l2.next.next = ListNode(5)
+
+printNode(mergeTwoLists(l1, l2))
