53f19fbb888d1f41241d9c87a0c525c4caf53329
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-01-15 20:08:20 +0100

    Add algorithm to analyze provided by neetcode

--- python/TrappingRainWater.py
+++ python/TrappingRainWater.py
@@ -20,7 +20,6 @@ def trap2(height):
     water = 0
     c = l
     while l < r:
-        print(height[c])
         area = min(max_l, max_r)
         water += area - height[c] if area-height[c] > 0 else 0
 
@@ -38,9 +37,25 @@ def trap2(height):
 
     return water
 
+# TODO: See how it works debugging
+def trap3(height):
+    if not height:
+        return 0
 
-
+    l, r = 0, len(height) - 1
+    leftMax, rightMax = height[l], height[r]
+    res = 0
+    while l < r:
+        if leftMax < rightMax:
+            l += 1
+            leftMax = max(leftMax, height[l])
+            res += leftMax - height[l]
+        else:
+            r -= 1
+            rightMax = max(rightMax, height[r])
+            res += rightMax - height[r]
+    return res
 
 
 height = [0,1,0,2,1,0,1,3,2,1,2,1]
-print(trap2(height))
+print(trap3(height))
