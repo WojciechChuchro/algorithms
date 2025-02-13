569b838f6699162b501f8f0ca371d51616751368
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-02-13 20:59:38 +0100

    Setup a algorithm

---
+++ Java/NeetCode/ReorderLinkedList.java
@@ -0,0 +1,20 @@
+package Java.NeetCode;
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+
+public class ReorderLinkedList {
+    public static void main(String[] args) {
+
+    }
+
+    public void reorderList(ListNode head) {
+
+    }
+}
