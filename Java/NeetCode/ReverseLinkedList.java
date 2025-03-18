adbe5a51aebd6054022ab53887d305c868c13c0c
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-03-18 18:34:15 +0100

    Reverse linked list using recursion

--- Java/NeetCode/ReverseLinkedList.java
+++ Java/NeetCode/ReverseLinkedList.java
@@ -2,6 +2,11 @@ package Java.NeetCode;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
 public class ReverseLinkedList {
     @Test
     public void testReverse() {
@@ -10,13 +15,41 @@ public class ReverseLinkedList {
         list.next.next = new ListNode(1);
         list.next.next.next = new ListNode(12);
         list.next.next.next.next = new ListNode(13);
-        printList(list);
-        ListNode ln = ReverseLinkedList.reverse(list);
-        System.out.println("reversed");
 
+        printList(list);
+//        ListNode ln = ReverseLinkedList.reverse(list);
+        ListNode ln = ReverseLinkedList.reverseRecursion(list);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        System.out.println("reversed");
         printList(ln);
+        System.setOut(originalOut);
+
+        String actualOutput = outputStream.toString().trim();
+
+        String expectedOutput = "reversed\n13 12 1 5 10";
+
+        assertEquals(expectedOutput, actualOutput);
+
     }
 
+    public static ListNode reverseRecursion(ListNode head) {
+
+        if (head == null) return null;
+
+        ListNode newHead = head;
+
+        if(head.next!= null) {
+            newHead =  reverseRecursion(head.next);
+            head.next.next = head;
+        }
+        head.next = null;
+
+        return newHead;
+    }
 
     public static ListNode reverse(ListNode head) {
         ListNode prev = null;
@@ -35,7 +68,8 @@ public class ReverseLinkedList {
 
 
     public static void printList(ListNode head) {
-        System.out.println(head.val);
+        System.out.print(head.val);
+        System.out.print(" ");
 
         if(head.next == null) {
             return;
