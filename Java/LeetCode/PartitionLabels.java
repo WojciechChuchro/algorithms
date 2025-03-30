3effc0083c8a943743e3f17cf70f211335760746
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-03-30 15:12:58 +0200

    code format

--- Java/LeetCode/PartitionLabels.java
+++ Java/LeetCode/PartitionLabels.java
@@ -8,19 +8,19 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         int arr[] = new int[26];
 
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             arr[s.charAt(i) - 'a'] = i;
         }
 
         int start = 0;
         int end = 0;
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             end = Math.max(end,
                     arr[s.charAt(i) - 'a']
             );
 
-            if( i == end) {
-                list.add(i - start+ 1);
+            if (i == end) {
+                list.add(i - start + 1);
                 start = i + 1;
             }
         }
