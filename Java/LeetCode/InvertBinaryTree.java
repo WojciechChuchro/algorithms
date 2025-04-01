ab14713b07bb2d9af7442c6b09092f3721f7dac6
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-04-01 16:13:33 +0200

    feat: implement invert tree algorithm

---
+++ Java/LeetCode/InvertBinaryTree.java
@@ -0,0 +1,33 @@
+package Java.LeetCode;
+
+
+public class InvertBinaryTree {
+
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+        public TreeNode invertTree(TreeNode root) {
+            if(root == null) {
+                return root;
+            }
+
+            TreeNode temp = root.right;
+            root.right = root.left;
+            root.left = temp;
+
+            invertTree(root.left);
+            invertTree(root.right);
+
+            return root;
+        }
+}
