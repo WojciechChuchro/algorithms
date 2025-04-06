1d6919b05f64c27b7b728eb466ecb95b0264ff89
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-04-06 10:58:38 +0200

    feat: implement reverse linked list

---
+++ leetcode/java/206_ReverseLinkedList.java
@@ -0,0 +1,26 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class Solution {
+    public ListNode reverseList(ListNode head) {
+      if(head == null) {
+        return head;
+      }
+      
+      ListNode l = null;
+      ListNode r = head;
+
+      while(r != null) {
+        ListNode temp = r.next;
+        r.next = l;
+        l = r;
+        r = temp;
+      }
+
+      return l;
+    }
+}
